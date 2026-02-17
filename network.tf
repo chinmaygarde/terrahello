8004080f44b413675ff65e914c6ad7d08c1c2d6b
Chinmay Garde <chinmaygarde@gmail.com>
2026-02-16 19:01:40 -0800

    Add NSG association to allow SSH.

--- network.tf
+++ network.tf
@@ -21,6 +21,29 @@ resource "azurerm_public_ip" "linux1_public_ip" {
   sku                 = "Standard"
 }
 
+resource "azurerm_network_security_group" "allow_ssh" {
+  name                = "allow_ssh"
+  location            = azurerm_resource_group.terrahello.location
+  resource_group_name = azurerm_resource_group.terrahello.name
+
+  security_rule {
+    name                       = "allow_ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "allow_ssh" {
+  network_interface_id = azurerm_network_interface.vm.id
+  network_security_group_id = azurerm_network_security_group.allow_ssh.id
+}
+
 resource "azurerm_network_interface" "vm" {
   name = "vm"
   location = azurerm_resource_group.terrahello.location
